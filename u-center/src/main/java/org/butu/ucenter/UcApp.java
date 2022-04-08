0ffcd5377d1edf45d93ea908c3cee7774929538d
BUTUbird <1781894948@qq.com>
2022-04-08 23:35:05 +0800

    Initial commit

---
+++ u-center/src/main/java/org/butu/ucenter/UcApp.java
@@ -0,0 +1,18 @@
+package org.butu.ucenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: MoYu
+ * @description: 用户中心应用启动类
+ * @packagename: org.butu.ucenter
+ * @author: BUTUbird
+ * @date: 2022-04-08 23:32
+ **/
+@SpringBootApplication
+public class UcApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UcApp.class, args);
+    }
+}
